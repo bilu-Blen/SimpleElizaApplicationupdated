f91609cff99a6b862bbf465276628f3416672f61
bilu-Blen <azblen1@gmail.com>
2018-10-11 17:03:28 -0400

    vbhhvhv

--- src/me/Blen/Eliza.java
+++ src/me/Blen/Eliza.java
@@ -1,5 +1,8 @@
 package me.Blen;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Eliza {
     private String inputedQuestion;
 
@@ -17,6 +20,29 @@ public class Eliza {
 
     public String askUser(){
         return " Enter you question here or Q to quit: ";
+    }
+    public String showRandomQualifier(){
+        ArrayList<String> randomQualifierArr = new ArrayList<>();
+
+        randomQualifierArr.add("Why do you say that ");
+        randomQualifierArr.add("You seem to think that ");
+        randomQualifierArr.add("So, you are concerned that ");
+
+        int index = new Random().nextInt(randomQualifierArr.size());
+        String randomQualifier = randomQualifierArr.get(index);
+        return randomQualifier;
+    }
+
+    public String showRandomHedges(){
+        ArrayList<String> randomHedgesArr = new ArrayList<>();
+
+        randomHedgesArr.add("Please tell me more ");
+        randomHedgesArr.add("Many of my patients tell me the same thing ");
+        randomHedgesArr.add("It is getting late, maybe we had better quit ");
+
+        int index = new Random().nextInt(randomHedgesArr.size());
+        String randomHedges = randomHedgesArr.get(index);
+        return randomHedges;
     }
 
 
--- src/me/Blen/Main.java
+++ src/me/Blen/Main.java
@@ -1,15 +1,16 @@
 package me.Blen;
 
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import static java.lang.String.join;
 
 public class Main {
     static Eliza eliza = new Eliza();
+    static ArrayList<String> newString = new ArrayList<>();
+    static ArrayList<String> historyArray = new ArrayList<>();
+
 
     public static void main(String[] args) {
 	// write your code here
@@ -27,58 +28,54 @@ public class Main {
             //user input
             Scanner scan = new Scanner(System.in);
             eliza.setUserQuestion(scan.nextLine());
+            historyArray.add(eliza.getUserQuestion());
 
             if(eliza.getUserQuestion().equalsIgnoreCase("i am feeling great")){
                 break;
             }else if( eliza.getUserQuestion().equalsIgnoreCase("q")){
+                System.out.println("Lets show you the history of your chat\n");
+
+                for(String eachArray : historyArray){
+                    System.out.print(eachArray);
+                }
 
             }else{
-                System.out.println(eliza.getUserQuestion());
+                mainMethod();
+            }
+    }
+    }
+
+    public static void mainMethod(){
+        int index = new Random().nextInt(100);
+        if(index%2==0){
+            String test = eliza.showRandomHedges();
+            System.out.print(test);
+            historyArray.add(test);
+        }else{
+            String test2 = eliza.showRandomQualifier();
+            System.out.print(test2);
+            historyArray.add(test2);
+
+            String[] userQuesArr = eliza.getUserQuestion().split(" ");
+
+            for( String eachString : userQuesArr){
+                eachString =  eachString.replaceAll("\\bi\\b", " you ").replaceAll("\\bme\\b", "you").replaceAll("\\bmy\\b", "your".replaceAll("\\bam\\b", "are"));
+                newString.add(eachString);
             }
 
-           String[] userQuesArr = eliza.getUserQuestion().split(" ");
-
-           showRandomQualifier();
-           for(String eachString : userQuesArr){
-
-
-               String replacedQues;
-                String nonReplaced;
-                eachString =  eachString.replaceAll("\\bi\\b", " you ").replaceAll("\\bme\\b", "you")
-                                            .replaceAll("\\bmy\\b", "your".replaceAll("\\bam\\b", "are"));
-
-               eliza.setUserQuestion( eachString );
-
-
-               eachString = eachString + " ";
-               String newString = join(" ", eachString);
-           }
-
-
-//           Pattern pat = Pattern.compile("are you");
-//           Matcher mat = pat.matcher(eliza.getUserQuestion());
-//
-//           if(mat.find()){
-//               System.out.println("found it");
-//           }else{
-//               System.out.println("not found");
-//           }
-
+            for(int i=0; i<newString.size(); i++)
+            {
+                if(newString.get(i).equalsIgnoreCase("are") && newString.get(i+1).equalsIgnoreCase("you"))
+                {
+                    newString.set(i,"am");
+                    newString.set(i+1,"I");
+                }
+                System.out.print(newString.get(i) + " ");
+                historyArray.add(newString.get(i) + " ");
+            }
+        }
 
     }
 
-    }
-
-    public static void showRandomQualifier(){
-        ArrayList<String> randomQualifierArr = new ArrayList<>();
-
-        randomQualifierArr.add("Why do you say that ");
-        randomQualifierArr.add("You seem to think that ");
-        randomQualifierArr.add("So, you are concerned that ");
-
-        int index = new Random().nextInt(randomQualifierArr.size());
-        String randomQualifier = randomQualifierArr.get(index);
-        System.out.print(randomQualifier);
-    }
 
 }
